7bfd9aa201171555c82025dba7409401f5d45177
wolfram74 <phaugen@gmail.com>
2016-05-14 10:59:33 -0500

    verified testing environment

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-
+  describe 'meta' do
+    it 'can run tests' do
+      expect(true).to eq(true)
+    end
+  end
 end
