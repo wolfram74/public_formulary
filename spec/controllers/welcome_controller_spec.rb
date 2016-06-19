2d805b58eed9887f23dabbb8796f9166613d0265
wolfram74 <phaugen@gmail.com>
2016-06-19 15:18:38 -0500

    included devise in rspec set up

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,8 +2,23 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
   describe 'meta' do
+    render_views
     it 'can run tests' do
       expect(true).to eq(true)
     end
+    it 'can return the home page' do
+      get :index
+      expect(response.status).to eq(200)
+      expect(response.body).to include('welcome')
+    end
+  end
+  describe 'authentication' do
+    xit 'can tell when a user is authenticated' do
+
+    end
+    xit 'can tell when a user isn\'t authenticated' do
+
+    end
+
   end
 end
